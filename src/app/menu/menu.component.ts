ba4f961e6b4291d56e5d9c2a6a93f2c6b3719d4c
David Allen <davetvmusic@hotmail.com>
2020-05-11 20:16:55 +1000

    code clean up

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,36 +1,28 @@
-import { Component, EventEmitter, OnInit, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
   public showMenu: boolean = false;
 
   @Input() menuItems: Array<string>;
   @Output() public emitMenuSelection = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
   toggleMenu(): void {
     this.showMenu = !this.showMenu;
   }
 
-  // emit the selected item and close the menu
   public selectItem(item: string): void {
     this.emitMenuSelection.emit(item);
     this.toggleMenu();
   }
 
   public closeMenu(): void {
-    if (this.showMenu) {
-      this.toggleMenu();
-    }
+    this.showMenu = false;
   }
 
 }
